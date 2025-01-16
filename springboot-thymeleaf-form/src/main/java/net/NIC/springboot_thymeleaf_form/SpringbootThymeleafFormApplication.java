bc29040464082bbafb974f41548f812e0b84c7fd
bshemphang <bshemphang@gmail.com>
2025-01-16 11:22:18 +0530

    Initial commit for Spring Boot project

---
+++ springboot-thymeleaf-form/src/main/java/net/NIC/springboot_thymeleaf_form/SpringbootThymeleafFormApplication.java
@@ -0,0 +1,13 @@
+package net.NIC.springboot_thymeleaf_form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafFormApplication.class, args);
+	}
+
+}
---
+++ springboot-thymeleaf-form/src/test/java/net/NIC/springboot_thymeleaf_form/SpringbootThymeleafFormApplicationTests.java
@@ -0,0 +1,13 @@
+package net.NIC.springboot_thymeleaf_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
